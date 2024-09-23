ace6ad4bf187cd04e1eaade7fce192d016af8178
Lavina <lvi321p@gmail.com>
2024-09-23 23:41:16 +0530

    Position Master and UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import * as XLSX from "xlsx";
+
+const fileNames = [
+  "cashflow",
+  "secDetail",
+  "redemption",
+  "subsecinfo",
+  "transaction",
+  "stockmasterV3",
+  "stockmasterV2",
+  "ratingmaster",
+  "marketprice",
+  "subposition",
+  "position",
+];
 
 const App = () => {
   const [file1, setFile1] = useState(null);
@@ -9,9 +23,12 @@ const App = () => {
 
   const [marketprice, setmarketprice] = useState(null);
 
+  const [lastSystemDate, setlastSystemDate] = useState(null);
+
   const [systemDate, setSystemDate] = useState(null);
   const [from, setFrom] = useState(null);
   const [to, setTo] = useState(null);
+  const [file, setFile] = useState("");
 
   const downloadXLSX = (data, filename) => {
     const worksheet = XLSX.utils.json_to_sheet(data);
@@ -36,55 +53,62 @@ const App = () => {
     setmarketprice(e.target.files[0]);
   };
 
+  useEffect(() => {
+    axios
+      .get("/api/systemdate")
+      .then(({ data }) => {
+        console.log(data);
+        if (data.status) {
+          const result = data.data;
+          console.log(result.SystemDate);
+          setlastSystemDate(result.SystemDate.split("T")[0]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const handleDownload = async () => {
-    if (!from) {
+    if (!file) {
+      return alert("Please select a file");
+    } else if (!from) {
       return alert("Please select from date");
     } else if (!to) {
-      return alert("Please select to Date");
+      return alert("Please select to date");
     }
 
     try {
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/download",
-        {
-          from,
-          to,
-        }
-      );
+      const { data } = await axios.post("/api/download", {
+        from,
+        to,
+        file,
+      });
 
       if (data.status) {
-        downloadXLSX(data.data);
-
-        // alert("Result file downloaded successfully");
+        downloadXLSX(data.data, `${file}.xlsx`);
+      } else {
+        alert(data.message);
       }
-    } catch (err) {
-      console.log(err);
-      alert("Something went wrong");
+    } catch (error) {
+      // console.log(err);
+      alert(error.response.data.message);
     }
   };
 
-  const handleSubmit = async (file) => {
+  const handleResultAndStockmaster = async () => {
     if (!systemDate) {
       return alert("Please select settlement date");
     }
 
     try {
       console.log(systemDate);
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/subsecinfo",
-        {
-          system_date: systemDate,
-        }
-      );
+      const { data } = await axios.post("/api/subsecinfo", {
+        system_date: systemDate,
+      });
 
       if (data.status) {
-        if (file === "stockmaster") {
-          downloadXLSX(data.stockmaster, "Stockmaster.xlsx");
-        } else {
-          downloadXLSX(data.subsecinfo, "PriceMaster.xlsx");
-        }
-
-        alert("Result file downloaded successfully");
+        console.log("Result file calculated successfully");
       }
     } catch (err) {
       console.log(err);
@@ -98,17 +122,14 @@ const App = () => {
     }
 
     try {
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/subposition",
-        {
-          system_date: systemDate,
-        }
-      );
+      const { data } = await axios.post("/api/subposition", {
+        system_date: systemDate,
+      });
 
       if (data.status) {
-        downloadXLSX(data.subposition, "SubPosition.xlsx");
+        // downloadXLSX(data.subposition, "SubPosition.xlsx");
 
-        alert("Result file downloaded successfully");
+        console.log("Sub-Position calculated successfully");
       }
     } catch (err) {
       console.log(err);
@@ -122,17 +143,14 @@ const App = () => {
         return alert("Please select settlement date");
       }
 
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/position",
-        {
-          system_date: systemDate,
-        }
-      );
+      const { data } = await axios.post("/api/position", {
+        system_date: systemDate,
+      });
 
       if (data.status) {
-        downloadXLSX(data.position, "Position.xlsx");
+        // downloadXLSX(data.position, "Position.xlsx");
 
-        alert("Result file downloaded successfully");
+        console.log("Position calculated successfully");
       }
     } catch (err) {
       console.log(err);
@@ -149,18 +167,14 @@ const App = () => {
     formData.append("file", marketprice);
 
     try {
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/marketprice",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const { data } = await axios.post("/api/marketprice", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
       if (data.status) {
-        downloadXLSX(data.marketprices, "MarketPrices.xlsx");
-        // alert("Cashflow file uploaded successfully");
+        //   downloadXLSX(data.marketprices, "MarketPrices.xlsx");
+        alert("MarketPrice file uploaded successfully");
       }
     } catch (err) {
       console.log(err);
@@ -177,15 +191,11 @@ const App = () => {
     formData.append("file", file1);
 
     try {
-      const { data } = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/cashflowupload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const { data } = await axios.post("/api/cashflowupload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
       if (data.status) {
         alert("Cashflow file uploaded successfully");
       }
@@ -193,6 +203,7 @@ const App = () => {
       alert("Failed to upload file.");
     }
   };
+
   const handleSecurityUpload = async () => {
     if (!file2) {
       return alert("Please Upload File 2");
@@ -202,15 +213,11 @@ const App = () => {
     formData.append("file", file2);
 
     try {
-      const response = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/securityupload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axios.post("/api/securityupload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
       if (response.data.status) {
         alert("Files uploaded successfully");
       }
@@ -228,15 +235,11 @@ const App = () => {
     formData.append("file", file3);
 
     try {
-      const response = await axios.post(
-        "http://gplank-test-eb-backend.ap-south-1.elasticbeanstalk.com/stockmasterupload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axios.post("/api/stockmasterupload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
       if (response.data.status) {
         alert("Files uploaded successfully");
       }
@@ -245,12 +248,24 @@ const App = () => {
     }
   };
 
+  const handleSubmit = async () => {
+    try {
+      await handleResultAndStockmaster();
+      await handleSubPosition();
+      await handlePosition();
+      setlastSystemDate(systemDate);
+      alert("Data Calculated");
+    } catch (error) {
+      return alert(error.message);
+    }
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen p-4 gap-4 bg-slate-100">
-      <h2 className="text-2xl text-black font-bold">Upload Excel File</h2>
-      <div className="flex gap-6 w-full">
-        <div className="w-1/2 flex flex-col gap-4 bg-slate-900 p-4 rounded-lg justify-around items-center">
-          <div className="flex justify-between gap-6">
+    <div className="flex flex-col justify-center items-center w-full bg-slate-900 h-screen p-4 gap-4 bg-slate-100">
+      <h2 className="text-2xl text-white font-bold">Upload Excel File</h2>
+      <div className="flex gap-4 w-full">
+        <div className="w-1/2 flex flex-col gap-4 p-4 rounded-lg justify-around items-center">
+          <div className="flex justify-between gap-4">
             <div className="flex flex-col gap-4 items-center">
               <input
                 type="file"
@@ -278,8 +293,8 @@ const App = () => {
             </button>
           </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-4 bg-slate-900 p-4 rounded-lg justify-around items-center">
-          <div className="flex justify-between gap-6">
+        <div className="w-1/2 flex flex-col gap-4 p-4 rounded-lg justify-around items-center">
+          <div className="flex justify-between gap-4">
             <div className="flex flex-col gap-4 items-center">
               <input
                 type="file"
@@ -308,7 +323,7 @@ const App = () => {
             </button>
           </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-4 bg-slate-900 p-4 rounded-lg justify-around items-center">
+        <div className="w-1/2 flex flex-col gap-4 p-4 rounded-lg justify-around items-center">
           <div className="justify-between ">
             <div className="flex flex-col gap-4 items-center">
               <input
@@ -336,64 +351,8 @@ const App = () => {
             </button>
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center gap-6 w-full">
-        <div className=" flex flex-col justify-around items-center gap-4 bg-slate-900 p-6 rounded-lg">
-          <div className="text-xl ">Submit System Date for Calculation</div>
-          <div className="flex items-center justify-around gap-4 w-full">
-            <div className="flex flex-col gap-2 items-center">
-              <input
-                type="date"
-                name=""
-                id="systemdate"
-                className="p-2 px-4 border-white border-2 rounded-md text-white font-bold"
-                onChange={(e) => setSystemDate(e.target.value)}
-              />
-            </div>
-            <button
-              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
-              onClick={() => handleSubmit("subsecinfo")}
-            >
-              Download Subsecinfo
-            </button>
-            <button
-              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
-              onClick={() => handleSubmit("stockmaster")}
-            >
-              Download Stockmaster
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center gap-6 w-full">
-        <div className=" flex flex-col justify-around items-center gap-4 bg-slate-900 p-6 rounded-lg">
-          <div className="text-xl ">Sub-Position Calculation</div>
-          <div className="flex items-center justify-around gap-4 w-full">
-            <button
-              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
-              onClick={handleSubPosition}
-            >
-              Download SubPosition
-            </button>
-          </div>
-        </div>
-        <div className=" flex flex-col justify-around items-center gap-4 bg-slate-900 p-6 rounded-lg">
-          <div className="text-xl ">Position Calculation</div>
-          <div className="flex items-center justify-around gap-4 w-full">
-            <button
-              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
-              onClick={handlePosition}
-            >
-              Download Position
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center gap-6 w-full">
-        <div className=" flex flex-col justify-around items-center gap-4 bg-slate-900 p-6 rounded-lg">
-          <div className="text-xl ">MarketPrices Calculation</div>
-
-          <div className="flex items-center justify-around gap-6 w-full">
+        <div className="w-1/2 flex flex-col gap-4 p-4 rounded-lg justify-around items-center">
+          <div className="justify-between ">
             <div className="flex flex-col gap-4 items-center">
               <input
                 type="file"
@@ -412,12 +371,95 @@ const App = () => {
                 Upload MarketPrice File
               </label>
             </div>
+          </div>
+          <div className="w-full flex justify-center">
             <button
-              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
+              className="bg-lime-300 p-2 px-4 text-black font-bold rounded-lg"
               onClick={handleMarketPrice}
             >
-              Download MarketPrices
+              Upload
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-4 w-full">
+        <div className=" flex flex-col justify-around items-center gap-4 p-6 rounded-lg">
+          <div className="text-xl ">Submit System Date for Calculation</div>
+          {lastSystemDate && (
+            <div className="text-xl ">Last System Date : {lastSystemDate}</div>
+          )}
+          <div className="flex items-center justify-around gap-4 w-full">
+            <div className="flex flex-col gap-2 items-center">
+              <input
+                type="date"
+                name=""
+                id="systemdate"
+                className="p-2 px-4 border-white border-2 rounded-md text-black font-bold"
+                onChange={(e) => setSystemDate(e.target.value)}
+              />
+            </div>
+
+            <button
+              className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-4 w-full">
+        <div className=" flex flex-col justify-around items-center gap-4 p-6 rounded-lg">
+          <div className="text-xl ">Download File Data</div>
+          <div className="flex items-center justify-around gap-4 w-full p-2">
+            <div className="flex gap-2">
+              <div className="p-2 flex flex-col gap-2">
+                <div>From</div>
+                <input
+                  type="date"
+                  name=""
+                  id=""
+                  className="p-2 px-4 border-white border-2 rounded-md text-black font-bold"
+                  onChange={(e) => setFrom(e.target.value)}
+                />
+              </div>
+              <div className="p-2 flex flex-col gap-2">
+                <div>To</div>
+                <input
+                  type="date"
+                  name=""
+                  id=""
+                  className="p-2 px-4 border-white border-2 rounded-md text-black font-bold"
+                  onChange={(e) => setTo(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 ">
+              <div>File</div>
+              <select
+                name=""
+                id=""
+                className="p-2 px-4 border-white border-2 rounded-md text-black font-bold"
+                onChange={(e) => setFile(e.target.value)}
+              >
+                <option value="">Select File</option>
+                {fileNames.map((fileName, index) => (
+                  <option key={index} value={fileName}>
+                    {fileName}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col gap-8 px-2 ">
+              <div></div>
+              <button
+                className="h-fit bg-orange-400 p-2 px-6 text-black font-bold rounded-md"
+                onClick={handleDownload}
+              >
+                Download
+              </button>
+            </div>
           </div>
         </div>
       </div>
